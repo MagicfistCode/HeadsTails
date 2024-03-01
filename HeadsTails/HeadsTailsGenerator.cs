e25d7e50d2241129732744ad42865a61b795faff
Dzaky <117564615+MagicfistCode@users.noreply.github.com>
2024-03-01 20:52:24 +0800

    Add project files.

---
+++ HeadsTails/HeadsTailsGenerator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadsTails
+{
+    public class HeadsTailsGenerator
+    {
+        //Creating heads or tails method where int "flip" can be changed
+        public void HeadsOrTails(int flips)
+        {
+            //declaring heads and tails variables
+            int heads = 0, tails = 0;
+
+            //creating a random class
+            var rand = new Random();
+
+            int i = 0;
+            //loop will keep going until the number of flips declared is reached
+            while (i < flips)
+            {
+                //creating a headsOrTails variable where it can be either 0 or 1
+                int headsOrTails = rand.Next(0, 2);
+                //if statement based on what happens if headsOrTails is 0 or 1
+                if (headsOrTails == 0)
+                {
+                    //heads is incremented by 1
+                    heads++;
+                    //a message saying the user got heads
+                    Console.WriteLine("You got heads!");
+                    //the user has to enter a key to flip again
+                    Console.ReadLine();
+                }
+                else
+                {
+                    //tails is incremented by 1
+                    tails++;
+                    //a message saying the user got tails
+                    Console.WriteLine("You got tails!");
+                    //the user has to enter a key to flip again
+                    Console.ReadLine();
+                }
+                //i is incremented by 1
+                i++;
+            }
+
+            //the user receives a message of how many times they got heads or tails
+            Console.WriteLine("You got heads " + heads + " times!\n" +
+                              "You got tails " + tails + " times!");
+        }
+    }
+}
---
+++ HeadsTails/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HeadsTails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating new HeadsTailsGenerator instance
+            HeadsTailsGenerator coinFlip = new HeadsTailsGenerator();
+
+            //flipping the coin 5 times
+            coinFlip.HeadsOrTails(5);
+
+            Console.ReadLine();
+        }
+    }
+}
